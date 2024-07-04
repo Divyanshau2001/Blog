3817857a53d6999a3d96a7c15cd31656c303b84a
Divyansh Sharma <divyanshnshu2001@gmail.com>
2024-07-04 23:04:37 +0530

    env setup and appwrite setup

---
+++ vite-project/src/config/config.js
@@ -0,0 +1,11 @@
+const conf = {
+    appwrtieUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwrtieProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwrtieDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwrtieCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwrtieBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+
+
+export default conf;
